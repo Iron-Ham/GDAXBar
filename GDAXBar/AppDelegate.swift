2f683acfc0aa73f36a55a1525debeacfcd8bc909
heshamsalman <hesham.salman@viewthespace.com>
2017-09-26 01:00:16 -0400

    Initial Commit

---
+++ GDAXBar/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  GDAXBar
+//
+//  Created by Hesham Salman on 9/26/17.
+//  Copyright © 2017 Hesham Salman. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ GDAXBar/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  GDAXBar
+//
+//  Created by Hesham Salman on 9/26/17.
+//  Copyright © 2017 Hesham Salman. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
